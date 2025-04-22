6857caa3827b7d76ba2f3a9e375e062e514de816
monyu-01 <opsu.0102@gmail.com>
2025-04-22 13:26:31 +0000

    [add]RSpec終了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user! , only: [:home,:about]
+  before_action :authenticate_user!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def after_sign_in_path_for(resource)
-    users_path
+    user_path(resource)
+  end
+
+  def after_sign_up_path_for(resource)
+    books_path
   end
 
   def after_sign_out_path_for(resource)
