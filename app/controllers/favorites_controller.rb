3ff366560f1df079176025719dbdf3c833437e85
monyu-01 <opsu.0102@gmail.com>
2025-04-24 03:04:06 +0000

    [add]課題3訂正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,7 +4,7 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to book_path(book)
+    redirect_to request.referer
     # respond_to do |format|
       # format.js
     # end
@@ -14,7 +14,7 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy if favorite
-    redirect_to book_path(book)
+    redirect_to request.referer
     # respond_to do |format|
       # format.js
       # format.html { redirect_to books_path }
