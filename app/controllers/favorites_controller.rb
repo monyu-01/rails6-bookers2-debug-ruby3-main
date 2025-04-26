61ad295daf46da10a932206ab983cf9fbbf13ad8
monyu-01 <opsu.0102@gmail.com>
2025-04-26 12:16:44 +0000

    [add]コメント機能非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,9 +5,6 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
     # redirect_to request.referer
-    # respond_to do |format|
-      # format.js
-    # end
   end
 
   def destroy
@@ -15,9 +12,5 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy if favorite
     # redirect_to request.referer
-    # respond_to do |format|
-      # format.js
-      # format.html { redirect_to books_path }
-    # end
   end
 end
