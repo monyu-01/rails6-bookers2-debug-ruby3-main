3832171b0eb816a3eac24c411565179e2cfe3eb6
monyu-01 <opsu.0102@gmail.com>
2025-04-26 10:08:19 +0000

    [start]課題6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,20 +1,20 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_to request.referer
+    # redirect_to request.referer
     # respond_to do |format|
       # format.js
     # end
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy if favorite
-    redirect_to request.referer
+    # redirect_to request.referer
     # respond_to do |format|
       # format.js
       # format.html { redirect_to books_path }
