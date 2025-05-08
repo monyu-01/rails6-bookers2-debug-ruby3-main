3c4f31a53098daa8629f2970d685f9fa754da332
monyu-01 <opsu.0102@gmail.com>
2025-05-08 04:49:57 +0000

    [add]通知機能追加終了

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:index,:show,:edit,:update]
+  resources :notifications, only: [:update]
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
